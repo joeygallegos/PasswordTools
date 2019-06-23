e87db8894a220cbb396c36e35574882d82bf28ba
Joey Gallegos <joey@joeygallegos.com>
2019-06-23 16:05:51 -0500

    Allow cipher method for config

--- app/encryption.php
+++ app/encryption.php
@@ -1,8 +1,9 @@
 <?php
 // The IV field will store the initialisation vector used for encryption. The storage requirements depend on the cipher and mode used. The password field will be hashed using a one-way password hash.
 if (!function_exists('openEncrypt')) {
-	function openEncrypt(string $string) {
-		$method = 'AES-256-CBC';
+	function openEncrypt(string $string)
+	{
+		$method = getenv('CIPHER_METHOD');
 		$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($method));
 		$key = substr(hash('sha256', $iv), 0, 16);
 
@@ -16,8 +17,9 @@ if (!function_exists('openEncrypt')) {
 }
 
 if (!function_exists('openDecrypt')) {
-	function openDecrypt(string $string, string $key, string $iv) {
-		$method = 'AES-256-CBC';
+	function openDecrypt(string $string, string $key, string $iv)
+	{
+		$method = getenv('CIPHER_METHOD');
 		return openssl_decrypt($string, $method, $key, $options = 0, $iv);
 	}
 }
